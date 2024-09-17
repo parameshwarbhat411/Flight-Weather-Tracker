8f341a89b4becc871c763cc282f960f45f53df05
parameshwarbhat411 <parameshwarbhat411>
2024-09-17 22:39:55 +0000

    Data ingestion updated

--- 01_Data_ingestion.py
+++ 01_Data_ingestion.py
@@ -4,6 +4,7 @@ import json
 from pyspark.sql.functions import col, to_timestamp
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType, TimestampType
 
+
 # COMMAND ----------
 
 api_key = dbutils.secrets.get('testscope','aviation-key')
@@ -16,11 +17,12 @@ base_url = "http://api.aviationstack.com/v1/flights"
 # Common parameters
 common_params = {
     'access_key': api_key,
-    'limit': 100,  # Adjust as per your API plan
+    'limit': 100,
+    'flight_status': 'active'
 }
 
 # Airports list without spaces
-airports = 'KATL,KDFW,KDEN,KORD,OMDB'
+airports = 'KATL'
 
 # Function to fetch flights
 def fetch_flights(params):
@@ -48,4 +50,55 @@ all_departures = fetch_flights(params_departures)
 
 # COMMAND ----------
 
-all_departures
+all_arrivals
+
+# COMMAND ----------
+
+all_arrivals = [{'flight_date': '2024-09-16',
+  'flight_status': 'active',
+  'departure': {'airport': 'Doha International',
+   'timezone': 'Asia/Qatar',
+   'iata': 'DOH',
+   'icao': 'OTHH',
+   'terminal': None,
+   'gate': None,
+   'delay': 27,
+   'scheduled': '2024-09-16T07:50:00+00:00',
+   'estimated': '2024-09-16T07:50:00+00:00',
+   'actual': '2024-09-16T08:17:00+00:00',
+   'estimated_runway': '2024-09-16T08:17:00+00:00',
+   'actual_runway': '2024-09-16T08:17:00+00:00'},
+  'arrival': {'airport': 'Hartsfield-jackson Atlanta International',
+   'timezone': 'America/New_York',
+   'iata': 'ATL',
+   'icao': 'KATL',
+   'terminal': 'I',
+   'gate': None,
+   'baggage': None,
+   'delay': 102,
+   'scheduled': '2024-09-16T15:55:00+00:00',
+   'estimated': '2024-09-16T15:55:00+00:00',
+   'actual': None,
+   'estimated_runway': None,
+   'actual_runway': None},
+  'airline': {'name': 'Qatar Airways', 'iata': 'QR', 'icao': 'QTR'},
+  'flight': {'number': '755',
+   'iata': 'QR755',
+   'icao': 'QTR755',
+   'codeshared': None},
+  'aircraft': {'registration': 'A7-ANA',
+   'iata': 'A35K',
+   'icao': 'A35K',
+   'icao24': '06A11D'},
+  'live': {'updated': '2024-09-16T17:14:53+00:00',
+   'latitude': 45.6905,
+   'longitude': -69.7638,
+   'altitude': 11582.4,
+   'direction': 224.34,
+   'speed_horizontal': 911.52,
+   'speed_vertical': 0,
+   'is_ground': False}}]
+
+# COMMAND ----------
+
+
